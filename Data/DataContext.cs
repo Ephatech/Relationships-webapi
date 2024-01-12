eeb6d4e11f1306c4274acc34f92979ea2cddf1cb
ephatech <ephunashamed@gmail.com>
2024-01-12 08:20:36 -0800

    .NET Relationships (Trial) Web API

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Relationships.Models;
+
+namespace Relationships.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users{ get; set; }
+
+        
+    }
+}
